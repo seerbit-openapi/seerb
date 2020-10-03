98c336e3e110bc1738f13f1d2fe3f74aaf8d7b6d
Halil Sambo <halilsambo@gmail.com>
2020-10-03 10:02:31 +0100

    build servee

---
+++ build/precache-manifest.7e6bb48ac71dfe87839719051f055414.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "673d3712051408839d0caa0290d60a37",
+    url: "/index.html"
+  },
+  {
+    revision: "23e20d3aff9e07c3140f",
+    url: "/static/css/main.b025e401.chunk.css"
+  },
+  {
+    revision: "bf62119ab83c9b1f08c7",
+    url: "/static/js/2.5d56066c.chunk.js"
+  },
+  {
+    revision: "23e20d3aff9e07c3140f",
+    url: "/static/js/main.555058b3.chunk.js"
+  },
+  {
+    revision: "74901d7983a77f4e4b8b",
+    url: "/static/js/runtime~main.b19b91af.js"
+  },
+  {
+    revision: "7699f64b3d25f50aa28eef6520af707b",
+    url: "/static/media/FaktPro-Blond.7699f64b.ttf"
+  },
+  {
+    revision: "21be8bf6dee5a2db4da348139797e7f6",
+    url: "/static/media/FaktPro-Light.21be8bf6.ttf"
+  },
+  {
+    revision: "f7da5fea55903009a39e2acfc21130f3",
+    url: "/static/media/FaktPro-Normal.f7da5fea.ttf"
+  },
+  {
+    revision: "8bed3bf58911a461b92a6dfc40418efe",
+    url: "/static/media/FaktPro-SemiBold.8bed3bf5.ttf"
+  }
+]);
